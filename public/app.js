9d938c755f5fc619c1b4eda866fbb3139b017c5f
Edward Hong <edward.hong527@gmail.com>
2021-05-06 18:50:41 +1200

    L17 Rendering an HTML Template

--- public/app.js
+++ public/app.js
@@ -1,27 +1,15 @@
 import { Invoice } from './classes/Invoice.js';
 import { Payment } from './classes/Payment.js';
-// let docOne: HasFormatter
-// let docTwo: HasFormatter
-// docOne = new Invoice('yoshi', 'web work', 250)
-// docTwo = new Payment('mario', 'plumbing work', 200)
-// let docs: HasFormatter[] = []
-// docs.push(docOne)
-// docs.push(docTwo)
-// console.log(docs)
-// const invOne = new Invoice('mario', 'work on the mario website', 250)
-// const invTwo = new Invoice('luigi', 'work on the luigi website', 300)
-// let invoices: Invoice[] = []
-// invoices.push(invOne)
-// invoices.push(invTwo)
-// invoices.forEach(inv => {
-//   console.log(inv.client, inv.amount, inv.format())
-// })
+import { ListTemplate } from './classes/ListTemplate.js';
 var form = document.querySelector('.new-item-form');
 //inputs
 var type = document.querySelector('#type');
 var tofrom = document.querySelector('#tofrom');
 var details = document.querySelector('#details');
 var amount = document.querySelector('#amount');
+// list temaplete instance
+var ul = document.querySelector('ul');
+var list = new ListTemplate(ul);
 form.addEventListener('submit', function (e) {
     e.preventDefault();
     var doc;
@@ -31,5 +19,5 @@ form.addEventListener('submit', function (e) {
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    list.render(doc, type.value, 'end');
 });
