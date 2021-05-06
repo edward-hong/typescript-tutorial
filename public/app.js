e6b8bd26a1ae1ffe636bf1b38ae3ba49feb08212
Edward Hong <edward.hong527@gmail.com>
2021-05-06 18:21:54 +1200

    L14 Modules

--- public/app.js
+++ public/app.js
@@ -1,19 +1,4 @@
-"use strict";
-// classes
-var Invoice = /** @class */ (function () {
-    // readonly client: string
-    // private details: string
-    // public amount: number
-    function Invoice(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    Invoice.prototype.format = function () {
-        return this.client + " owes \u00A3" + this.amount + " for " + this.details;
-    };
-    return Invoice;
-}());
+import { Invoice } from './classes/Invoice.js';
 var invOne = new Invoice('mario', 'work on the mario website', 250);
 var invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 var invoices = [];
---
+++ public/classes/Invoice.js
@@ -0,0 +1,16 @@
+// classes
+var Invoice = /** @class */ (function () {
+    // readonly client: string
+    // private details: string
+    // public amount: number
+    function Invoice(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes \u00A3" + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+export { Invoice };
