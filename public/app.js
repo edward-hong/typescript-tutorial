f0897b671b4a860c53e63f95aa7f0d9bae075cf2
Edward Hong <edward.hong527@gmail.com>
2021-05-06 18:59:21 +1200

    L18 Generics

--- public/app.js
+++ public/app.js
@@ -1,3 +1,14 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 import { Invoice } from './classes/Invoice.js';
 import { Payment } from './classes/Payment.js';
 import { ListTemplate } from './classes/ListTemplate.js';
@@ -21,3 +32,20 @@ form.addEventListener('submit', function (e) {
     }
     list.render(doc, type.value, 'end');
 });
+// Generics
+var addUID = function (obj) {
+    var uid = Math.floor(Math.random() * 100);
+    return __assign(__assign({}, obj), { uid: uid });
+};
+var docOne = addUID({ name: 'yoshi', age: 40 });
+var docThree = {
+    uid: 1,
+    resourceName: 'person',
+    data: { name: 'Edward' },
+};
+var docFour = {
+    uid: 2,
+    resourceName: 'shoppingList',
+    data: ['bread', 'milk', 'toilet roll'],
+};
+console.log(docThree, docFour);
--- src/app.ts
+++ src/app.ts
@@ -28,3 +28,33 @@ form.addEventListener('submit', (e: Event) => {
 
   list.render(doc, type.value, 'end')
 })
+
+// Generics
+
+const addUID = <T extends { name: string }>(obj: T) => {
+  let uid = Math.floor(Math.random() * 100)
+  return { ...obj, uid }
+}
+
+let docOne = addUID({ name: 'yoshi', age: 40 })
+
+// with interfaces
+interface Resource<T> {
+  uid: number
+  resourceName: string
+  data: T
+}
+
+const docThree: Resource<object> = {
+  uid: 1,
+  resourceName: 'person',
+  data: { name: 'Edward' },
+}
+
+const docFour: Resource<string[]> = {
+  uid: 2,
+  resourceName: 'shoppingList',
+  data: ['bread', 'milk', 'toilet roll'],
+}
+
+console.log(docThree, docFour)
